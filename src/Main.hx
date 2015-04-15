ad0eea366e4abbe47093aa1263e3a6a2612b0853
Robert Konrad <robert@ktx-software.com>
2015-04-16 01:38:15 +0200

    Fixed everything.

--- src/Main.hx
+++ src/Main.hx
@@ -5,8 +5,16 @@ import haxe.io.Path;
 class Main {
 	static function main() {
 		var io = Path.join([Sys.getCwd(), "Tools", "iojs", "iojs.exe"]);
+
 		var args = Sys.args();
-		args.unshift(Path.join(["Tools", "khamake", "khamake.js"]));
-		Sys.exit(Sys.command(io, args));
+		args.unshift(Path.join([Sys.getCwd(), "Tools", "khamake", "khamake.js"]));
+
+		var project = Path.normalize(args.pop());
+		args.push("from=" + project);
+		args.push("to=" + Path.join([project, "build"]));
+		args.push("haxe=" + Path.normalize(Sys.getEnv("HAXEPATH")));
+		args.push("kha=" + Path.normalize(Sys.getCwd()));
+
+		Sys.exit(Sys.command('"' + io + '"', args));
 	}
 }
